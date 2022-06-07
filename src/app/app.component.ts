c82ae0f87e16b64b65f9edd228892b19b00c0a1a
NalsigStlo <nicolas.lagos.s@mail.pucv.cl>
2022-06-06 22:36:03 -0400

    Se saca importaciones no ocupadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [NgbCarouselConfig]
 })
 export class AppComponent {
   title = 'PunchStarter';
